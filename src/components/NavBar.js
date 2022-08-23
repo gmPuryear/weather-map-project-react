9a08a9b9145a51680653a6a9930dc9541d8f82e5
George Puryear <gm.puryear@yahoo.com>
2022-08-23 16:33:44 -0500

    completed navbar with all styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import nav4Logo from '../images/Channel_4-3.png'
 
 export default function NavBar() {
 
 
     return (
-        <div>
-            <div className="nav_ribbon navBar">hello</div>
-            <div></div>
+        <div className="nav-wrapper">
+            <div className="nav_ribbon navBar">
+                <div className="navBar-words"> Stay Classy
+                    <img className="nav_logo" src={nav4Logo} alt="this is the channel 4 logo from Anchorman"/>
+                    San Diego
+                </div>
+            </div>
         </div>
     )
 }
