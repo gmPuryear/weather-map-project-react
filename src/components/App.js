667bd85e0230eb9441ef1f162fde6e8c8fdbc1a9
George Puryear <gm.puryear@yahoo.com>
2022-08-22 21:11:35 -0500

    added ribbon to navbar and changed background color of app

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import '../css/app.css';
+import NavBar from "./NavBar";
+
+// COMPONENTS
+  // - Navbar
+  // - Mapbox
+  // - Weather card
+  // - Weather info for card
+
+function App() {
+  return (
+    <>
+    <NavBar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function NavBar() {
+
+
+    return (
+        <div>
+            <div className="nav_ribbon navBar">hello</div>
+            <div></div>
+        </div>
+    )
+}
